e19cd7bc5a380fa22947938067b76a1c7ab14f3c
ksmith <killian.smith@hpcnow.com>
2023-04-16 19:12:42 +0200

    changed quit/reload bindings

--- config.def.h
+++ config.def.h
@@ -63,22 +63,23 @@ static const int natural_scrolling = 0;
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[] = { "alacritty", NULL };
-static const char *menucmd[] = { "bemenu-run", NULL };
+//static const char *termcmd[] = { "alacritty", NULL };
+static const char *termcmd[] = { "foot", NULL };
+//static const char *menucmd[] = { "bemenu-run", NULL };
+static const char *menucmd[] = { "flaunchmenu", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 	/* modifier                  key                 function        argument */
-+	{ MODKEY|WLR_MODIFIER_SHIFT|WLR_MODIFIER_CTRL, XKB_KEY_M,        restartdwl,     {0} },
 	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,     spawn,          {.v = termcmd} },
+	{ MODKEY,                    XKB_KEY_Return,     spawn,          {.v = termcmd} },
 	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
 	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
 	{ MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
 	{ MODKEY,                    XKB_KEY_d,          incnmaster,     {.i = -1} },
 	{ MODKEY,                    XKB_KEY_h,          setmfact,       {.f = -0.05} },
 	{ MODKEY,                    XKB_KEY_l,          setmfact,       {.f = +0.05} },
-	{ MODKEY,                    XKB_KEY_Return,     zoom,           {0} },
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,     zoom,           {0} },
 	{ MODKEY,                    XKB_KEY_Tab,        view,           {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_C,          killclient,     {0} },
 	{ MODKEY,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
@@ -86,7 +87,7 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
-	{ MODKEY,                    XKB_KEY_e,         togglefullscreen, {0} },
+	{ MODKEY,                    XKB_KEY_e,          togglefullscreen, {0} },
 	{ MODKEY,                    XKB_KEY_0,          view,           {.ui = ~0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_parenright, tag,            {.ui = ~0} },
 	{ MODKEY,                    XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
@@ -101,8 +102,8 @@ static const Key keys[] = {
 	TAGKEYS(          XKB_KEY_6, XKB_KEY_asciicircum,                5),
 	TAGKEYS(          XKB_KEY_7, XKB_KEY_ampersand,                  6),
 	TAGKEYS(          XKB_KEY_8, XKB_KEY_asterisk,                   7),
-	TAGKEYS(          XKB_KEY_9, XKB_KEY_parenleft,                  8),
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Q,          quit,           {0} },
+	{ MODKEY|WLR_MODIFIER_SHIFT|WLR_MODIFIER_CTRL, XKB_KEY_Q,        quit,           {0} },
++	{ MODKEY|WLR_MODIFIER_SHIFT,                   XKB_KEY_Q,        restartdwl,     {0} },
 
 	/* Ctrl-Alt-Backspace and Ctrl-Alt-Fx used to be handled by X server */
 	{ WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_Terminate_Server, quit, {0} },
--- run-dwl.sh
+++ run-dwl.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 DWL="$HOME/software/dwl/result/bin/dwl"
+INFO="$HOME/.cache/dwl_info" 
+FLAG="/tmp/restart_dwl"
 
 if [ -z $XDG_RUNTIME_DIR ]; then
     export XDG_RUNTIME_DIR=/tmp/xdg-runtime-$(id -u)
@@ -9,9 +11,12 @@ fi
 
 #exec ${DWL} > "$HOME/.cache/dwl_info" &
 
-DO=true
-while ${DO} || [ -f /tmp/restart_dwl ]; do
-    ${DO}=false
-    rm -rf /tmp/restart_dwl > /dev/null 2>&1
-    ${DWL} > "$HOME/.cache/dwl_info" 
+[ -f "${FLAG}" ] || touch "${FLAG}"
+
+while [ -f "${FLAG}" ]; do
+    rm -rf "${FLAG}" > /dev/null 2>&1
+    swaybg -c "#77dd88" -i "$HOME/dotfiles/wallpapers/oni2.png" -m fit &
+    ${DWL} > "${INFO}"
+    #swaybg -c "#444444" -i "$HOME/dotfiles/wallpapers/oni2.png" -m fit &
+    #swaybg -o \* -i "$HOME/dotfiles/wallpapers/oni2.png" -m fill 
 done
